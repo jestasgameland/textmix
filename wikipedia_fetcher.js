182f8fdec5e4b0133fdc517195d3e32751da39e6
Brendon Albertson <albertsonbrendon@gmail.com>
2021-02-22 13:31:44 -0500

    Add files via upload
    - refactored undo() so it just saves a history of all previous states of answerSentence.innerHTML- This is a much simpler way to undo and also works for cloze modes.
    - Added support for generating cloze exercises by removing the 100 most frequent words in English 
    - Fixed bugs in cloze modes (articles, prepositions, and grammar words)
    - added cleanSentence() to index.html and cleanText() to wikipedia_fetcher.js - these remove newline characters, which can cause bugs.
    - updated parts of code to use innerHTML vs. innerText vs. textContent

--- wikipedia_fetcher.js
+++ wikipedia_fetcher.js
@@ -1,5 +1,16 @@
 
 
+	function cleanText(txt) {
+		try {	//if there aren't any periods or newlines to replace, this prevents an error 
+			txt = txt.replace(".  ",". "); //standardize # of spaces after period to one
+			txt = txt.replace(".\n",". "); 
+			txt = txt.replace( /[\r\n]+/gm, "" ); //replace all carriage returns, etc.
+		} catch{}; 
+
+		return txt;
+	}
+
+
 	function wikiSearch(searchTerm, language, divId, doThisNext) {
 
 		//divId is div where you want the results displayed
@@ -14,14 +25,18 @@
 
 		req.onreadystatechange = function() {
 			if(this.readyState==4 && this.status==200) {
+
 				var data = JSON.parse(this.response);
 				var key = Object.keys(data.query.pages)[0];
 				var text = data.query.pages[key].extract;
 				if (text == undefined) {
 					text = "<br><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sorry, nothing found.</p>";
+				}
+				else {
+					cleanText(text);
 				};
 
-				document.getElementById(divId).innerHTML = text;
+				document.getElementById(divId).innerText = text;
 
 				doThisNext();
 			};
